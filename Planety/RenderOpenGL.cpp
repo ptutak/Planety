2ca2c0d76c2f25a007b875f64e895ac65bafe734
ptutak <p.tutak@outlook.com>
2016-11-07 21:56:44 +0100

    opengl

--- Planety/RenderOpenGL.cpp
+++ Planety/RenderOpenGL.cpp
@@ -5,13 +5,6 @@ enum
 	ASPECT_1_1, // aspekt obrazu 1:1
 	WIRE_SPHERE, // kula
 	WIRE_CONE, // sto¿ek
-	WIRE_CUBE, // szeœcian
-	WIRE_TORUS, // torus
-	WIRE_DODECAHEDRON, // dwunastoœcian
-	WIRE_TEAPOT, // czajnik
-	WIRE_OCTAHEDRON, // oœmioœcian
-	WIRE_TETRAHEDRON, // czworoœcian
-	WIRE_ICOSAHEDRON, // dwudziestoœcian
 	EXIT // wyjœcie
 };
 
@@ -23,15 +16,13 @@ GLdouble centerx = 0;
 GLdouble centery = 0;
 GLdouble centerz = 0;
 
-int aspect = FULL_WINDOW;
+int aspect = ASPECT_1_1;
 
-int object = WIRE_SPHERE;
+GLdouble left = -10.0;
+GLdouble right = 10.0;
+GLdouble bottom = -10.0;
 
-const GLdouble left = -10.0;
-const GLdouble right = 10.0;
-const GLdouble bottom = -10.0;
-
-const GLdouble top = 10.0;
+GLdouble top = 10.0;
 GLdouble _near = 50.0;
 GLdouble _far = 155.0;
 
@@ -39,9 +30,11 @@ GLdouble scale = 1.0;
 
 GLdouble rotatex = 0.0;
 GLdouble rotatey = 0.0;
+GLdouble rotatez = 0.0;
 
 GLdouble translatex = 0.0;
 GLdouble translatey = 0.0;
+GLdouble translatez = 0.0;
 
 int button_state = GLUT_UP;
 
@@ -52,39 +45,41 @@ int button_x, button_y;
 int argcp = 0;
 char* argv = nullptr;
 
-void Cube3x3()
-{
-	glutWireCube(1.0);
-	glTranslated(1.0, 0.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(0.0, -1.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(-1.0, 0.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(-1.0, 0.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(0.0, 1.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(0.0, 1.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(1.0, 0.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(1.0, 0.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(-1.0, -1.0, 0.0);
-}
+void origin(void) {
+	glLineWidth(3.0);
+	glBegin(GL_LINES);
+	glColor3f(1.0, 0.0, 0.0);
 
+	glVertex3d(0.0, 0.0, 0.0);
+	glVertex3d(1.0, 0.0, 0.0);
+	glVertex3d(1.0, 0.0, 0.0);
+	glVertex3d(0.7, 0.3, 0.0);
+	
+	glColor3f(0.0, 0.0, 1.0);
 
-void Cube2x2()
-{
-	glutWireCube(1.0);
-	glTranslated(1.0, 0.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(0.0, -1.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(-1.0, 0.0, 0.0);
-	glutWireCube(1.0);
-	glTranslated(0.5, 0.5, 0.0);
+	glVertex3d(0.0, 0.0, 0.0);
+	glVertex3d(0.0, 1.0, 0.0);
+	glVertex3d(0.0, 1.0, 0.0);
+	glVertex3d(0.0, 0.7, 0.3);
+
+	glColor3f(0.0, 1.0, 0.0);
+
+	glVertex3d(0.0, 0.0, 1.0);
+	glVertex3d(0.0, 0.0, 0.0);
+	glVertex3d(0.0, 0.0, 1.0);
+	glVertex3d(0.3, 0.0, 0.7);
+
+	glEnd();
+	glColor3f(1.0, 0.0, 0.0);
+	glRasterPos3d(1.0, -0.25, -0.25);
+	glutBitmapCharacter(GLUT_BITMAP_9_BY_15, static_cast<int>('x'));
+	glColor3f(0.0, 0.0, 1.0);
+	glRasterPos3d(-0.25, 1.0, -0.25);
+	glutBitmapCharacter(GLUT_BITMAP_9_BY_15, static_cast<int>('y'));
+	glColor3f(0.0, 1.0, 0.0);
+	glRasterPos3d(-0.25, -0.25, 1.0);
+	glutBitmapCharacter(GLUT_BITMAP_9_BY_15, static_cast<int>('z'));
+	glLineWidth(1.0);
 }
 
 void display(void) {
@@ -93,66 +88,31 @@ void display(void) {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	glColor3f(0.0, 0.0, 0.0);
-
 	glTranslated(0, 0, -(_near + _far) / 2);
 
-	glScaled(scale, scale, scale);
+	glPushMatrix();
 
+	glScaled(3.0, 3.0, 3.0);
+	glRotated(rotatex, 1.0, 0.0, 0.0);
+	glRotated(rotatey, 0.0, 1.0, 0.0);
+	glRotated(rotatez, 0.0, 0.0, 1.0);
+	origin();
+	glPopMatrix();
 
 	glRotated(rotatex, 1.0, 0.0, 0.0);
 	glRotated(rotatey, 0.0, 1.0, 0.0);
+	glRotated(rotatez, 0.0, 0.0, 1.0);
+
+	glTranslated(translatex, translatey, translatez);
 	
-	Cube2x2();
-	glColor3f(0.0, 1.0, 0.0);
-	glutWireCube(1.0);
+	glScaled(scale, scale, scale);
 
-	glColor3f(1.0, 0.0, 0.0);
 
-	glBegin(GL_LINES);
 
-	glVertex3d(0.0, 0.0, 0.0);
-	glVertex3d(1.0, 0.0, 0.0);
+
+	glColor3f(0.0, 0.0, 0.0);
+	glutWireCylinder(1.0, 2.0, 30, 5);
 	
-	glVertex3d(1.0, 0.0, 0.0);
-	glVertex3d(0.7, 0.3, 0.0);
-
-	glVertex3d(0.0, 0.0, 0.0);
-	glVertex3d(0.0, 1.0, 0.0);
-
-	glVertex3d(0.0, 0.0, 1.0);
-	glVertex3d(0.0, 0.0, 0.0);
-
-
-	glEnd();
-
-
-	glTranslated(10.0, 0.0, 0.0);
-
-	glColor3f(0.0, 1.0, 0.0);
-
-
-	glTranslated(translatex, translatey, 0);
-
-	glColor3f(0.0, 0.0, 1.0);
-
-	glBegin(GL_LINES);
-
-	glVertex3d(0.0, 0.0, 0.0);
-	glVertex3d(1.0, 0.0, 0.0);
-
-	glVertex3d(1.0, 0.0, 0.0);
-	glVertex3d(0.7, 0.3, 0.0);
-
-	glVertex3d(0.0, 0.0, 0.0);
-	glVertex3d(0.0, 1.0, 0.0);
-
-	glVertex3d(0.0, 0.0, 0.0);
-	glVertex3d(0.0, 0.0, 1.0);
-
-	glEnd();
-
-
 	glFlush();
 	glutSwapBuffers();
 }
@@ -163,17 +123,11 @@ void reshape(int width, int height) {
 
 	glLoadIdentity();
 
-	if (aspect == ASPECT_1_1)
-	{
-		// wysokoœæ okna wiêksza od wysokoœci okna
+	if (aspect == ASPECT_1_1) {
 		if (width < height && width > 0)
 			glFrustum(left, right, bottom * height / width,top * height / width, _near, _far);
-		else
-
-			// szerokoœæ okna wiêksza lub równa wysokoœci okna
-			if (width >= height && height > 0)
+		else if (width >= height && height > 0)
 				glFrustum(left * width / height, right * width / height, bottom, top, _near, _far);
-
 	}
 	else
 		glFrustum(left, right, bottom, top, _near, _far);
@@ -182,64 +136,71 @@ void reshape(int width, int height) {
 
 void keyboard(unsigned char key, int x, int y) {
 	if (key == '+')
-		scale +=0.1;
+		rotatez +=1.5;
 	if (key == '-')
-		scale -= 0.1;
-	if (scale < 0.0)
-		scale = 0.0;
+		rotatez -= 1.5;
 	if (key == 27)
 		exit(0);
-
 	reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
 }
 
 void specialKeys(int key, int x, int y)
 {
-	switch (key)
-	{
-		
+	switch (key) {
 	case GLUT_KEY_LEFT:
-		rotatey -= 1;
+		rotatey -= 1.5;
 		break;
 	case GLUT_KEY_RIGHT:
-		rotatey += 1;
+		rotatey += 1.5;
 		break;
-
 	case GLUT_KEY_UP:
-		rotatex -= 1;
+		rotatex -= 1.5;
 		break;
 	case GLUT_KEY_DOWN:
-		rotatex += 1;
+		rotatex += 1.5;
+		break;
+	case GLUT_KEY_PAGE_UP:
+		translatez += 1.0;
+		break;
+	case GLUT_KEY_PAGE_DOWN:
+		translatez -= 1.0;
 		break;
 	}
-
-	// odrysowanie okna
 	reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
 }
 
 void mouseButton(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON)
 	{
-		// zapamiêtanie stanu lewego przycisku myszki
 		button_state = state;
-
-		// zapamiêtanie po³o¿enia kursora myszki
 		if (state == GLUT_DOWN)
 		{
 			button_x = x;
 			button_y = y;
 		}
 	}
+	else if ((button == 3) || (button == 4)) {
+		if (state == GLUT_UP) 
+			return;
+		if (button == 3)
+			scale += 0.1;
+		if (button == 4)
+			scale -= 0.1;
+		if (scale < 0)
+			scale = 0.0;
+		reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+	}
 }
 
 void mouseMotion(int x, int y) {
 	if (button_state == GLUT_DOWN)
 	{
-		translatex += 1.1 *(right - left) / glutGet(GLUT_WINDOW_WIDTH) *(x - button_x);
+		translatex += 1.4 *(right - left) / glutGet(GLUT_WINDOW_WIDTH) *(x - button_x);
 		button_x = x;
-		translatey += 1.1 *(top - bottom) / glutGet(GLUT_WINDOW_HEIGHT) *(button_y - y);
+		translatey += 1.4 *(top - bottom) / glutGet(GLUT_WINDOW_HEIGHT) *(button_y - y);
 		button_y = y;
-		glutPostRedisplay();
+		reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+
 	}
 }
 void menu(int value)
@@ -257,61 +218,6 @@ void menu(int value)
 		aspect = ASPECT_1_1;
 		reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
 		break;
-
-		// kula
-	case WIRE_SPHERE:
-		object = WIRE_SPHERE;
-		display();
-		break;
-
-		// cylinder
-	case WIRE_CONE:
-		object = WIRE_CONE;
-		display();
-		break;
-
-		// szeœcian
-	case WIRE_CUBE:
-		object = WIRE_CUBE;
-		display();
-		break;
-
-		// torus
-	case WIRE_TORUS:
-		object = WIRE_TORUS;
-		display();
-		break;
-
-		// dwunastoœcian
-	case WIRE_DODECAHEDRON:
-		object = WIRE_DODECAHEDRON;
-		display();
-		break;
-
-		// czajnik
-	case WIRE_TEAPOT:
-		object = WIRE_TEAPOT;
-		display();
-		break;
-
-		// oœmioœcian
-	case WIRE_OCTAHEDRON:
-		object = WIRE_OCTAHEDRON;
-		display();
-		break;
-
-		// czworoœcian
-	case WIRE_TETRAHEDRON:
-		object = WIRE_TETRAHEDRON;
-		display();
-		break;
-
-		// dwudziestoœcian
-	case WIRE_ICOSAHEDRON:
-		object = WIRE_ICOSAHEDRON;
-		display();
-		break;
-
 		// wyjœcie
 	case EXIT:
 		exit(0);
@@ -333,15 +239,6 @@ void startRendering(gravityField* gravField) {
 	glutAddMenuEntry("aspekt 1:1", ASPECT_1_1);
 	glutAddMenuEntry("cale okno", FULL_WINDOW);
 	int menuObject = glutCreateMenu(menu);
-	glutAddMenuEntry("Kula", WIRE_SPHERE);
-	glutAddMenuEntry("Stozek", WIRE_CONE);
-	glutAddMenuEntry("Szescian", WIRE_CUBE);
-	glutAddMenuEntry("Torus", WIRE_TORUS);
-	glutAddMenuEntry("Dwunastoscian", WIRE_DODECAHEDRON);
-	glutAddMenuEntry("Czajnik", WIRE_TEAPOT);
-	glutAddMenuEntry("Osmioscian", WIRE_OCTAHEDRON);
-	glutAddMenuEntry("Czworoscian", WIRE_TETRAHEDRON);
-	glutAddMenuEntry("Dwudziestoscian", WIRE_ICOSAHEDRON);
 	// menu g³ówne
 	glutCreateMenu(menu);
 	glutAddSubMenu("Aspekt obrazu", menuAspect);
--- Planety/RenderOpenGL.h
+++ Planety/RenderOpenGL.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <GL\freeglut.h>
+#include <GL\GL.h>
 #include "PlanetyClasses.h"
 
 void startRendering(gravityField* gravField);
