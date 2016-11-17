49fd55e8fddfcb69f045510612d7ca484e4b0e34
ptutak <p.tutak@outlook.com>
2016-11-17 01:28:59 +0100

    bez komentarzy

--- Planety/Main.cpp
+++ Planety/Main.cpp
@@ -3,28 +3,8 @@
 
 int main(void)
 {
-/*	std::mutex mutex;
-	gravityField* tmp = new gravityField;
-	tmp->addObject(new flyingObject{ "Ziemia",5.9722e+24,0,0,0,0,0,0,0 });
-	tmp->addObject(new flyingObject{ "Object1",1,0,6378000,0,0,0,7910,0 });
-	try {
-		startRendering(&tmp,&mutex);
-	}
-	catch (const threadExit& x) {
-		std::cout << x.what()<<std::endl;
-	}
-	catch (const std::exception& x) {
-		std::cout << x.what() << std::endl;
-	}
-	catch (...) {
-		std::cout << "nieoczekiwany wyjatek" << std::endl;
-		system("pause");
-		return 0;
-	}
-*/	
-	//	std::cout << "jestem tu, po wyjsciu z loop" << std::endl;
-
 	init();
+
 	system("pause");
 }
 	
